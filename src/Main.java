878d0d049a7d706b2443f2307d9b689c3635c0bf
Eric Carstensen <37225222+E-Carstensen@users.noreply.github.com>
2023-09-19 11:44:04 -0600

    Added filename user input

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,41 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
 
-        Path path = Paths.get("src", "test.csv");
-
-        if(!Files.exists(path)){
-            System.out.println("File Not Found... Exiting");
-            return;
-        }
-
-        String filePath = path.toAbsolutePath().toString();
+        String filePath =  getFileName();
         DataSet dataSet = new DataSet(filePath);
 
+        displayStats(dataSet);
+    }
+
+
+    public static void displayStats(DataSet dataSet){
         System.out.println("Number of Entries: " + dataSet.entries);
         System.out.println("Highest Value: " + dataSet.getHighestValue());
         System.out.println("Lowest Value: " + dataSet.getLowestValue());
         System.out.println("Number of Unique Wards: " + dataSet.countUniqueWards());
         System.out.println("Number of Assessment Classes: " + dataSet.countAssessmentClasses());
-
     }
 
+    public static String getFileName(){
+        System.out.print("CSV Filename: ");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        Path path = Paths.get("src", input);
+
+        if(!Files.exists(path)){
+            System.out.println("File Not Found... Exiting");
+            return "";
+        }
+
+        return path.toAbsolutePath().toString();
+    }
 
 }
---
+++ src/shortenCSV.py
@@ -0,0 +1,23 @@
+import csv
+
+def copy_first_n_lines(input_file_path, output_file_path, n):
+    try:
+        with open(input_file_path, 'r', newline='') as infile, open(output_file_path, 'w', newline='') as outfile:
+            reader = csv.reader(infile)
+            writer = csv.writer(outfile)
+            for i, row in enumerate(reader):
+                if i < n:
+                    writer.writerow(row)
+                else:
+                    break
+        print(f"{n} lines copied from '{input_file_path}' to '{output_file_path}'.")
+    except FileNotFoundError:
+        print(f"The file '{input_file_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+# Example usage:
+input_file_path = 'data.csv'  # Replace with your input CSV file path
+output_file_path = 'test.csv'  # Replace with the desired output CSV file path
+n = 5  # Replace with the number of lines you want to copy
+copy_first_n_lines(input_file_path, output_file_path, n)
